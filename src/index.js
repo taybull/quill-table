40673045333505c39953f042baf4e2531eb83920
lucap <luc@rissc.de>
2017-10-25 15:11:24 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -13,11 +13,6 @@ Container.order = [
     'td', 'tr', 'table'  // Must be higher
 ];
 
-Quill.register(TableCell, true);
-Quill.register(TableRow, true);
-Quill.register(Table, true);
-Quill.register(Contain, true);
-
 export {
     TableCell,
     TableRow,
