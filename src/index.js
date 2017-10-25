50066977405225b3a471d6c455e19b0de525bfd8
lucap <luc@rissc.de>
2017-10-25 17:42:22 +0200

    ...v111111111

--- src/index.js
+++ src/index.js
@@ -16,19 +16,21 @@ Container.order = [
 
 export {TableCell, TableRow, Table, Contain};
 
-export default function tableModule(quill, options) {
-    let toolbar = quill.getModule('toolbar');
-    toolbar.addHandler('table', function (value) {
-        return TableTrick.table_handler(value, quill);
-    });
-    let clipboard = quill.getModule('clipboard');
-    clipboard.addMatcher('TABLE', function (node, delta) {
-        return delta;
-    });
-    clipboard.addMatcher('TR', function (node, delta) {
-        return delta;
-    });
-    clipboard.addMatcher('TD', function (node, delta) {
-        return delta.compose(new Delta().retain(delta.length(), {td: node.getAttribute('table_id') + '|' + node.getAttribute('row_id') + '|' + node.getAttribute('cell_id')}));
-    });
+export class TableModule {
+    constructor(quill, options) {
+        let toolbar = quill.getModule('toolbar');
+        toolbar.addHandler('table', function (value) {
+            return TableTrick.table_handler(value, quill);
+        });
+        let clipboard = quill.getModule('clipboard');
+        clipboard.addMatcher('TABLE', function (node, delta) {
+            return delta;
+        });
+        clipboard.addMatcher('TR', function (node, delta) {
+            return delta;
+        });
+        clipboard.addMatcher('TD', function (node, delta) {
+            return delta.compose(new Delta().retain(delta.length(), {td: node.getAttribute('table_id') + '|' + node.getAttribute('row_id') + '|' + node.getAttribute('cell_id')}));
+        });
+    }
 }
